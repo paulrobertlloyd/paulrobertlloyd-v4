1a341079b034d74a30b7f1ad02ef8c168acc2a3e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-04-05 11:57:53 +0100

    Compute canonical URL

--- src/_content/_content.11tydata.js
+++ src/_content/_content.11tydata.js
@@ -3,16 +3,23 @@ const getWebmentions = require('../../lib/utils/get-webmentions');
 module.exports = {
   eleventyComputed: {
     page_title: data => data.title, // Value overridden for homepage
+    canonical_url: data => {
+      if (data.canonical && data.canonical.url) {
+        return data.canonical.url;
+      }
+
+      return data.app.url + data.page.url;
+    },
     share_image: data => {
       const {image} = data;
       if (image) {
         return `https://res.cloudinary.com/paulrobertlloyd/image/fetch/c_fill%2Cf_auto%2Cq_auto%2Cw_1200%2Ch_630/${data.app.url}${image}`;
       }
 
-      return `${data.app.url}${data.app.icon}`;
+      return data.app.url + data.app.icon;
     },
     webmentions: data => {
-      const url = `${data.app.url}${data.page.url}`;
+      const url = data.app.url + data.page.url;
       return getWebmentions(data.webmentions.children, url);
     }
   }
