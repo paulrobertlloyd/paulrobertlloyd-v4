71e7da7350276ed73a40b7fc5a2ee42c876c6ff3
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-04-05 02:04:20 +0100

    Compute articles related to projects

--- src/_content/projects/projects.11tydata.js
+++ src/_content/projects/projects.11tydata.js
@@ -4,5 +4,18 @@ module.exports = {
   tags: ['post', 'project'],
   vocab: 'entry',
   changefreq: 'monthly',
-  priority: 0.9
+  priority: 0.9,
+  eleventyComputed: {
+    related: data => {
+      const articles = data.collections.article;
+      return articles.filter(article => {
+        const {category} = article.data;
+        if (!category) {
+          return false;
+        }
+
+        return category.includes(data.title);
+      });
+    }
+  }
 };
