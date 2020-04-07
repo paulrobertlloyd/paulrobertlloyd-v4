df9d6a9d7fb6b5f54158ddb766a653495bbd6860
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-04-08 00:06:53 +0100

    Fix sitemap.xml and image URLs

--- src/_content/_content.11tydata.js
+++ src/_content/_content.11tydata.js
@@ -13,7 +13,7 @@ module.exports = {
     share_image: data => {
       const {image} = data;
       if (image) {
-        return `https://res.cloudinary.com/paulrobertlloyd/image/fetch/c_fill%2Cf_auto%2Cq_auto%2Cw_1200%2Ch_630/${data.app.url}${image}`;
+        return `https://res.cloudinary.com/paulrobertlloyd/image/fetch/c_fill,f_auto,q_auto,w_1200,h_630/${data.app.url}${image}`;
       }
 
       return data.app.url + data.app.icon;
