2ac9aac9290a0c25067657c6068969f25420ec60
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-04-27 21:13:10 +0100

    Refactor testimonials data

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,25 +1,11 @@
-module.exports = function () {
-  const testimonials = [{
-    content: 'Paul is fantastic to work with and a truly excellent designer. From communication, to testing, to presenting his work; he never ceases to go the extra mile. Paul has played a central role in a really exciting time for our product and has made such an impact – thank you, Paul.',
-    name: 'Emma Begg',
-    role: 'Product Owner',
-    org: 'CharityConnect'
-  }, {
-    content: 'Working on Fontdeck with Paul was a pleasure. He brought his usual meticulous attention in redesigning the site, resulting in a far more consistent and harmonious feel, along with numerous small yet significant improvements in usability. Ultimately Paul added the finesse the site was missing.',
-    name: 'Richard Rutter',
-    role: 'CEO',
-    org: 'Fontdeck'
-  }];
-
-  return testimonials.map(testimonial => {
-    return {
-      data: {
-        layout: 'testimonial',
-        content: testimonial.content,
-        name: testimonial.name,
-        role: testimonial.role,
-        org: testimonial.org
-      }
-    };
-  });
-};
+module.exports = [{
+  content: 'Paul is fantastic to work with and a truly excellent designer. From communication, to testing, to presenting his work; he never ceases to go the extra mile. Paul has played a central role in a really exciting time for our product and has made such an impact – thank you, Paul.',
+  name: 'Emma Begg',
+  role: 'Product Owner',
+  org: 'CharityConnect'
+}, {
+  content: 'Working on Fontdeck with Paul was a pleasure. He brought his usual meticulous attention in redesigning the site, resulting in a far more consistent and harmonious feel, along with numerous small yet significant improvements in usability. Ultimately Paul added the finesse the site was missing.',
+  name: 'Richard Rutter',
+  role: 'CEO',
+  org: 'Fontdeck'
+}];
