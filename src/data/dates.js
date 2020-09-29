f255984731ee0526bdf1769629afb9d38e5deb18
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-09-29 01:23:40 +0100

    Enable custom time zones on photo and note posts

--- src/data/dates.js
+++ src/data/dates.js
@@ -2,6 +2,6 @@ module.exports = {
   iso: 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX',
   date_short: 'dd MMM yyyy',
   date_long: 'dd MMMM yyyy',
-  datetime_full: 'dd MMMM yyyy, h:mm\'&nbsp;\'aaaaa\'m\'',
+  datetime_full: 'dd MMMM yyyy, h:mm\'&nbsp;\'aaaaa\'m\' zzz',
   time: 'h:mm\'&nbsp;\'aaaaa\'m\''
 };
