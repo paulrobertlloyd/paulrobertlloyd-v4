0775279e2fda561845cfa0b9e09d3d65d1f7df92
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-27 23:19:38 +0100

    Allow related events to be shown alongside trips

--- src/data/eleventyComputed.js
+++ src/data/eleventyComputed.js
@@ -16,8 +16,10 @@ module.exports = {
   summaryImageAlt: data => data.pageImage ? data.pageImage.alt : 'Logo',
   related: data => {
     const posts = data.collections.post;
+    const events = data.collections.event;
+    const relatedItems = posts.concat(events);
     const related = data.related ? data.related : [];
-    return posts.filter(post => related.includes(post.url));
+    return relatedItems.filter(post => related.includes(post.url));
   },
   webmentions: data => {
     const url = data.app.url + data.page.url;
