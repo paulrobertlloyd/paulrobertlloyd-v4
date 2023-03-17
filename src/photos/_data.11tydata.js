583d285dfedb8acfdf2b5ec81508384cc0cb4171
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-17 23:01:11 +0000

    Refactor photo collection

--- src/photos/_data.11tydata.js
+++ src/photos/_data.11tydata.js
@@ -1,15 +1,11 @@
 module.exports = {
   layout: 'photo',
-  syndicate: true,
-  type_prefix: 'p',
   permalink: 'photos/{{ page.fileSlug }}/',
-  tags: ['photo'],
   sitemap: {
     changefreq: 'monthly',
     priority: 0.8,
   },
+  syndicate: true,
+  type_prefix: 'p',
   vocabulary: 'entry',
-  eleventyComputed: {
-    page_title: '{% if photo.size > 1 -%}{{ photo.size }} photos{% else %}Photo{% endif %}: {{ date | date: dates.date_time }} · {{ app.name }}',
-  },
 };
