6fc567027d8234e3278b4a3dd3f92e4c4d451cc6
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-18 20:06:38 +0000

    Export filters

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const collections = require('./lib/collections/index.js');
+const filters = require('./lib/filters/index.js');
 
 module.exports = function (eleventy) {
   // Extensions
@@ -23,26 +24,11 @@ module.exports = function (eleventy) {
   eleventy.setDataFileBaseName('_data');
 
   // Filters
-  eleventy.addFilter('absolute_url', require('@11ty/eleventy-plugin-rss').absoluteUrl);
-  eleventy.addFilter('color', require('./lib/filters/color.js'));
-  eleventy.addFilter('contrast_with', require('./lib/filters/contrast-with.js'));
-  eleventy.addFilter('date_with_time_zone', require('./lib/filters/date-with-time-zone.js'));
-  eleventy.addFilter('excludes', require('./lib/filters/excludes.js'));
-  eleventy.addFilter('hostname', require('./lib/filters/hostname.js'));
-  eleventy.addFilter('includes', require('./lib/filters/includes.js'));
-  eleventy.addFilter('itinerary', require('./lib/filters/itinerary.js'));
-  eleventy.addFilter('map_url', require('./lib/filters/map-url.js'));
-  eleventy.addFilter('markdown', require('./lib/filters/markdown.js'));
-  eleventy.addFilter('percentage_of', require('./lib/filters/percentage-of.js'));
-  eleventy.addFilter('pluralise', require('./lib/filters/pluralise.js'));
-  eleventy.addFilter('sum', require('./lib/filters/sum.js'));
-  eleventy.addFilter('syndication_target', require('./lib/filters/syndication-target.js'));
-  eleventy.addFilter('template_content_to_feed_html', require('./lib/filters/template-content-to-feed-html.js'));
-  eleventy.addFilter('tokenize', require('./lib/filters/tokenize.js'));
-  eleventy.addFilter('trip_distance', require('./lib/filters/trip-distance.js'));
-  eleventy.addFilter('trip_emissions', require('./lib/filters/trip-emissions.js'));
-  eleventy.addFilter('trip_title', require('./lib/filters/trip-title.js'));
+  for (const [name, filter] of Object.entries(filters)) {
+    eleventy.addFilter(name, filter);
+  }
 
+  // Slugify filter
   const slugifyFilter = eleventy.getFilter('slugify');
   eleventy.addFilter('slugify', string => slugifyFilter(string, {
     customReplacements: [
