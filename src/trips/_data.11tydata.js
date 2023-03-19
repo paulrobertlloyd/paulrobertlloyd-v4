8d5c21c612f3cac300df8d95b2f76588e9a3c6c1
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-19 17:50:01 +0000

    New permalink format for trips

--- src/trips/_data.11tydata.js
+++ src/trips/_data.11tydata.js
@@ -1,7 +1,7 @@
 module.exports = {
   layout: 'trip',
+  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}{{ page.fileSlug }}/',
   type_prefix: 't',
   visibility: 'unlisted',
   vocabulary: 'event',
-  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}{{ page.fileSlug }}/',
 };
