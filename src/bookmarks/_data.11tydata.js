bf986406c6f491129fc170dcc87688afd19329ad
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-19 17:54:54 +0000

    New permalink format for bookmarks

--- src/bookmarks/_data.11tydata.js
+++ src/bookmarks/_data.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
   layout: 'bookmark',
-  permalink: 'bookmarks/{{ page.date | date: "%Y/%m" }}/{{ page.fileSlug }}/',
+  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}1/{{ page.fileSlug }}/',
   syndicate: true,
   type_prefix: 'b',
   vocabulary: 'entry',
