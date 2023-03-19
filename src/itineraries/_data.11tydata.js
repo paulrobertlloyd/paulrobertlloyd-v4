e9ae5b72d5961f11ea1423949592e4c37cbc016e
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-19 17:51:50 +0000

    New permalink format for itineraries

--- src/itineraries/_data.11tydata.js
+++ src/itineraries/_data.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
   layout: 'itinerary',
-  permalink: 'travel/{{ page.date | date: "%Y/%m" }}/{{ page.fileSlug }}/',
+  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}1/{{ page.fileSlug }}/',
   type_prefix: 'i',
   vocabulary: 'event',
 };
