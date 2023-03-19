5127a13d35d632d63f541094fb759d45eb47a6a0
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-19 17:50:21 +0000

    New permalink format for replies

--- src/replies/_data.11tydata.js
+++ src/replies/_data.11tydata.js
@@ -1,8 +1,7 @@
 module.exports = {
   layout: 'reply',
-  permalink: 'replies/{{ page.fileSlug }}/',
+  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}{{ page.fileSlug }}/',
   type_prefix: 'r',
   visibility: 'unlisted',
   vocabulary: 'entry',
-  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}{{ page.fileSlug }}/',
 };
