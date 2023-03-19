1fa556ab9edc0ac322fe87c9d033511dab839ec9
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-19 18:38:58 +0000

    New permalink format for photos

--- src/photos/_data.11tydata.js
+++ src/photos/_data.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
   layout: 'photo',
-  permalink: 'photos/{{ page.fileSlug }}/',
+  permalink: '{{ page.date | date: "%Y/%j" }}/{{ type_prefix }}{{ page.fileSlug }}/',
   syndicate: true,
   type_prefix: 'p',
   vocabulary: 'entry',
