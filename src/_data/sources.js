f28a6ec170f0c7d0d79ac2c44280236351aa1549
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-21 00:31:01 +0000

    Update source description for 24 ways

--- src/_data/sources.js
+++ src/_data/sources.js
@@ -1,6 +1,6 @@
 module.exports = [{
   title: '24 ways',
-  summary: 'The advent calendar for web geeks. I’m a frequent contributor and [redesigned the site in 2013](/2013/12/redesigning_24_ways).',
+  summary: 'The advent calendar for web geeks.',
   url: 'https://24ways.org',
 }, {
   title: 'a.green:focus',
