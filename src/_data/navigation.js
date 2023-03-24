d63532341dc6ff9e4f553d56d4a75856d865df9d
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-03-24 01:46:00 +0000

    Refactor navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -2,28 +2,28 @@ module.exports = {
   primary: [{
     label: 'Writing',
     url: '/articles/',
-    tag: 'article',
+    post_type: 'article',
   }, {
     label: 'Speaking',
     url: '/presentations/',
-    tag: 'presentation',
+    post_type: 'presentation',
   }, {
     label: 'Projects',
     url: '/projects/',
-    tag: 'project',
+    post_type: 'project',
   }],
   secondary: [{
     label: 'Notes',
     url: '/notes/',
-    tag: 'note',
+    post_type: 'note',
   }, {
     label: 'Photos',
     url: '/photos/',
-    tag: 'photo',
+    post_type: 'photo',
   }, {
     label: 'Bookmarks',
     url: '/bookmarks/',
-    tag: 'bookmark',
+    post_type: 'bookmark',
   }, {
     label: 'With Ukraine',
     url: 'https://www.withukraine.org/',
@@ -38,9 +38,11 @@ module.exports = {
   }, {
     label: 'Travel',
     url: '/travel/',
+    post_type: 'itinerary',
   }, {
     label: 'Events',
     url: '/events/',
+    post_type: 'event',
   }],
   social: [{
     label: 'Mastodon',
