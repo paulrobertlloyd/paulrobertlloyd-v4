26addde057abbb1e669d4977b51efa25f635edcf
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-10-02 00:17:04 +0100

    Generate route map images during build

--- eleventy.config.js
+++ eleventy.config.js
@@ -47,7 +47,7 @@ module.exports = function (eleventy) {
   // Shortcodes
   eleventy.addShortcode("icon", require("./lib/shortcodes/icon.js"));
   eleventy.addShortcode("image", require("./lib/shortcodes/image.js"));
-  eleventy.addShortcode("map", require("./lib/shortcodes/map.js"));
+  eleventy.addShortcode("route_map", require("./lib/shortcodes/route-map.js"));
 
   // Folder data
   eleventy.setDataFileBaseName("_data");
