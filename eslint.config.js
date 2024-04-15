80c8ab6d1427f7e79f6b98be7aa895eac0ff52b3
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-15 23:36:19 +0100

    Update to ESLint v9.0.0 and use flat file config

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import jsdoc from "eslint-plugin-jsdoc";
+import unicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  jsdoc.configs["flat/recommended"],
+  unicorn.configs["flat/recommended"],
+  {
+    ignores: ["www/**"],
+  },
+  {
+    languageOptions: { globals: { ...globals.node } },
+    rules: {
+      "jsdoc/no-undefined-types": [1, { definedTypes: ["NodeJS"] }],
+      "unicorn/filename-case": [
+        "warn",
+        {
+          case: "kebabCase",
+          ignore: ["eleventyComputed.js"],
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/assets/**/*.js"],
+    languageOptions: { globals: { ...globals.browser } },
+  },
+  prettier,
+];
