ed6cf4203a05a5fa7387a0ddd26f30ee11a1b5f9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-05-10 23:05:21 +0200

    Rename stylelint configuration file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,13 @@
-{
-  "extends": "stylelint-config-standard",
-  "plugins": ["stylelint-order"],
-  "rules": {
+export default {
+  extends: "stylelint-config-standard",
+  plugins: ["stylelint-order"],
+  rules: {
     "order/properties-alphabetical-order": true,
     "selector-class-pattern": [
       "^(?<block>(?:[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)(?<element>(?:__[a-z][a-z0-9]*(?:-[a-z0-9]+)*))?(?<modifier>(?:--[a-z][a-z0-9]*)(?:-[a-z0-9]+)*)?$",
       {
-        "resolveNestedSelectors": true
-      }
-    ]
-  }
-}
+        resolveNestedSelectors: true,
+      },
+    ],
+  },
+};
