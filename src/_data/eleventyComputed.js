eb785b9b56c00b51095906a71b1428cf04251a19
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-07-28 19:29:14 +0100

    Remove publications collection

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -8,17 +8,6 @@ export default {
   page_image: (data) => data.photo && data.photo[0],
   published: (data) => data?.start || data?.date || data.page.date,
   properties: ({ collections, eleventy, page, pkg, ...rest }) => rest,
-  publication: (data) => {
-    const { collections, canonical } = data;
-    const publication = collections.publication.find((item) => {
-      const { issn, uid } = item.data;
-      return (
-        (issn && canonical?.issn === issn) ||
-        (uid && canonical?.url?.includes(uid))
-      );
-    });
-    return publication?.data;
-  },
   featured_image: (data) =>
     data.featured
       ? `${data.featured.url}?tr=w-1200,h-630`
